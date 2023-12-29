3152da070a6103495d5151e4b6bb743153d75f5c
Elijah Camara <elijahjohncamara@icloud.com>
2023-12-29 15:06:22 -0500

    The code is much cleaner.

--- frontend/src/pages/Root.jsx
+++ frontend/src/pages/Root.jsx
@@ -1,12 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 const RootLayout = () => {
+    const navigation = useNavigation();
+
     return (
         <>
             <MainNavigation />
-            <Outlet />
+            {/* This loading is different than useState or useEffect state as it 
+            renders the loading symbole on the parrent component rather than the 
+            component being traveled to. */}
+            <main>
+                <Outlet />
+                {navigation.state === 'loading' && <p>Loading...</p>}
+            </main>
         </>
     )
 };
